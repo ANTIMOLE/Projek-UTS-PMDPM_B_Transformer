f4f2650ab8ce130916f5b9e8f128cab83d57473a
ANTIMOLE <angellokhara4672@gmail.com>
2024-10-25 16:10:21 +0700

    NEXT

--- MainStreamlit_B_Transformer.py
+++ MainStreamlit_B_Transformer.py
@@ -42,7 +42,7 @@ if selected == 'Klasifikasi':
               st.markdown("<h3 style='text-align : center' font-color: red> !!⚠️ An error occurred while reading the file ⚠️!! </h3>", unsafe_allow_html=True)
     # Lokasi model
     
-    model_path = 'BestModel_CLF_RFC_NamaSB.pkl'
+    model_path = 'BestModel_CLF_RFC_Transformer.pkl'
 
     if os.path.exists(model_path):
         with open(model_path, 'rb') as f:
@@ -123,8 +123,7 @@ if selected == 'Klasifikasi':
 if selected == 'Regresi':
     st.title('📈 Prediksi Harga Properti')
 
-    model_directory = r'E:\clone\Project'
-    model_path = os.path.join(model_directory, r'BestModel_REG_Ridge_NamaSB.pkl')
+    model_path = 'BestModel_REG_Ridge_Transformer.pkl'
 
     st.write('Untuk Inputan File dataset (csv) bisa menggunakan st.file_uploader')
     file = st.file_uploader('Masukkan File Dataset Anda', type=["csv", "txt"])
